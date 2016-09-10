9fb512542c2341bb235c36b4cc5d92d6d516915d
Hamza Developer <hamzadeveloper@hotmail.com>
2016-09-10 19:53:28 +0300

    Add Mouse Show
    add mouse show with static bool show and with update()

---
+++ Game/MouseShow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseShow : MonoBehaviour {
+
+    public static bool isOn = false;
+
+    void Update()
+    {
+        if (isOn)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+        }
+    }
+
+}
